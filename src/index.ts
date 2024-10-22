44f8c2a14f6933be0882a0915f2a24ebd5f7fc13
Michael Absolon <absolon.mm@gmail.com>
2024-10-22 18:22:30 +0200

    Use papi enums

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { createClient, FixedSizeBinary, SS58String } from "polkadot-api";
 import { getWsProvider } from "polkadot-api/ws-provider/node";
-import { darwinia } from "@polkadot-api/descriptors";
+import {
+  darwinia,
+  XcmV3Junction,
+  XcmV3Junctions,
+  XcmV3WeightLimit,
+  XcmVersionedLocation,
+} from "@polkadot-api/descriptors";
 import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
 import { ecdsaCreateDerive } from "@polkadot-labs/hdkd";
 import {
@@ -29,25 +35,19 @@ const start = async () => {
   );
 
   const tx = client.getTypedApi(darwinia).tx.XTokens.transfer({
-    currency_id: { type: "ForeignAsset", value: BigInt("1029") },
-    amount: BigInt("1000"),
-    dest: {
-      type: "V3",
-      value: {
-        parents: 1,
-        interior: {
-          type: "X2",
-          value: [
-            { type: "Parachain", value: 2000 },
-            {
-              type: "AccountId32",
-              value: { id: addr, network: undefined },
-            },
-          ],
-        },
-      },
-    },
-    dest_weight_limit: { type: "Unlimited", value: undefined },
+    currency_id: { type: "ForeignAsset", value: BigInt(1029) },
+    amount: BigInt(1000),
+    dest: XcmVersionedLocation.V3({
+      parents: 1,
+      interior: XcmV3Junctions.X2([
+        XcmV3Junction.Parachain(2000),
+        XcmV3Junction.AccountId32({
+          network: undefined,
+          id: addr,
+        }),
+      ]),
+    }),
+    dest_weight_limit: XcmV3WeightLimit.Unlimited(),
   });
 
   const hex = await tx.sign(signer);
