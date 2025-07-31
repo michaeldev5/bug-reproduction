1036db7280907e03c7f87b1f15b81f263992ee85
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2025-07-31 23:00:13 +0200

    Update to HydrationPaseo reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import { createClient } from "polkadot-api";
+import { Binary, createClient } from "polkadot-api";
 import "./App.css";
 import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
 import { getWsProvider } from "polkadot-api/ws-provider/web";
 
+import { getPolkadotSigner } from "polkadot-api/signer";
+import {
+  DEV_PHRASE,
+  entropyToMiniSecret,
+  mnemonicToEntropy,
+} from "@polkadot-labs/hdkd-helpers";
+import { sr25519CreateDerive } from "@polkadot-labs/hdkd";
+
+const createSr25519Signer = () => {
+  const miniSecret = entropyToMiniSecret(mnemonicToEntropy(DEV_PHRASE));
+  const derive = sr25519CreateDerive(miniSecret);
+  const aliceKeyPair = derive("//Alice");
+  return getPolkadotSigner(
+    aliceKeyPair.publicKey,
+    "Sr25519",
+    aliceKeyPair.sign
+  );
+};
+
 function App() {
-  const getFromRpc = async () => {
+  const createAndSignTx = async () => {
     const client = createClient(
       withPolkadotSdkCompat(
-        getWsProvider("wss://asset-hub-polkadot-rpc.dwellir.com")
+        getWsProvider("wss://paseo-rpc.play.hydration.cloud")
       )
     );
 
-    const value = await client._request("state_getStorage", [
-      "0x5fbc5c7ba58845ad1f1a9a7c5bc12fad",
-    ]);
+    console.log("Client created");
 
-    console.log("value", value);
+    const tx = await client
+      .getUnsafeApi()
+      .txFromCallData(
+        Binary.fromHex(
+          "0x8900040000000000e8890423c78a000000000000000004010200a10f0100246044e82dcb430908830f90e8c668b02544004d66eab58af5124b953ef57d3700"
+        )
+      );
 
-    client.destroy();
+    console.log("Transaction created:");
+
+    console.log("Signing transaction...");
+
+    const signer = createSr25519Signer();
+
+    console.log("Signer created");
+
+    const result = await tx.sign(signer);
+
+    console.log("Transaction result:", result);
   };
 
   const onClick = () => {
-    getFromRpc();
+    createAndSignTx();
   };
 
   return (
