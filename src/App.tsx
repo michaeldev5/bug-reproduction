4b54c894e3aea769ffd117f48c721ec00a097960
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2026-04-13 13:45:10 +0200

    Add dedot chainhead reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { Keyring } from "@polkadot/api";
 import "./App.css";
 import { DedotClient, WsProvider as DedotWsProvider } from "dedot";
 
 const WS_URL = "wss://acala-rpc-0.aca-api.network";
 
-export const createSr25519Signer = (path: string) => {
-  const keyring = new Keyring({ type: "sr25519" });
-  return keyring.addFromUri(path);
-};
-
 function App() {
   const onClickDedot = async () => {
-    const signer = createSr25519Signer("//Alice");
-
-    const api = await DedotClient.new(new DedotWsProvider(WS_URL));
-
-    const tx = api.tx.polkadotXcm.transferAssets();
-    console.log(await tx.sign(signer));
+    await DedotClient.new(new DedotWsProvider(WS_URL));
   };
 
   return (
