59cad8319fdff008a56b5fa3d91e4843aed3957e
Michael Absolon <79721475+michaeldev5@users.noreply.github.com>
2026-04-13 13:41:18 +0200

    Add dedot chainhead reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
+import { Keyring } from "@polkadot/api";
 import "./App.css";
-import { ApiPromise, WsProvider } from "@polkadot/api";
+import { DedotClient, WsProvider as DedotWsProvider } from "dedot";
 
-const WS_URL = "wss://asset-hub-polkadot.ibp.network";
+const WS_URL = "wss://acala-rpc-0.aca-api.network";
+
+export const createSr25519Signer = (path: string) => {
+  const keyring = new Keyring({ type: "sr25519" });
+  return keyring.addFromUri(path);
+};
 
 function App() {
-  const onClickPjs = async () => {
-    const api = await ApiPromise.create({
-      provider: new WsProvider(WS_URL),
-    });
+  const onClickDedot = async () => {
+    const signer = createSr25519Signer("//Alice");
 
-    const customXcm = {
-      V4: [
-        // Multiple instructions here
-        // I am adding one just as an example
-        {
-          WithdrawAsset: [
-            {
-              id: {
-                parents: 1,
-                interior: { Here: null },
-              },
-              fun: {
-                Fungible: "1000000000",
-              },
-            },
-          ],
-        },
-      ],
-    };
+    const api = await DedotClient.new(new DedotWsProvider(WS_URL));
 
-    const objHex = api.createType("XcmVersionedXcm", customXcm).toHex();
-    console.log(objHex);
+    const tx = api.tx.polkadotXcm.transferAssets();
+    console.log(await tx.sign(signer));
   };
 
   return (
     <>
-      <h1>PJS reproduction</h1>
+      <h1>PAPI reproduction</h1>
       <div className="card">
-        <button onClick={onClickPjs}>Click me PJS</button>
+        <button onClick={onClickDedot}>Click me DEDOT</button>
       </div>
     </>
   );
--- src/config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "@reactive-dot/core";
-import { LedgerWallet } from "@reactive-dot/wallet-ledger";
-
-export const config = defineConfig({
-  chains: {},
-  wallets: [new LedgerWallet()],
-});
